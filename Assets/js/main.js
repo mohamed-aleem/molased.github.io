f5bb1c9f816b81430c6c7387268dd05919f0d59a
molased <molased@gmail.com>
2015-07-22 10:19:06 +0200

    - added content dynamic to the flow of the website - now has consistent animation or transition between the preview and content view - this commit before adding dummy object to do all intermediate scrolltop things

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,8 +1,46 @@
 $(function () {
 
+    var currentInterviewContainer;
+
+
+    $('#close-content-button').click(function () {
+
+        $('.interview-content').removeClass('animate');
+        $('#previews-container').removeClass('no-scroll');
+
+        setTimeout(function() {
+            currentInterviewContainer.removeClass('animate');
+            $('.interview-content').html('');
+        }, 500);
+    });
+
+
     $('.interview-container').click(function () {
 
-        $(this).toggleClass('animate');
+        currentInterviewContainer = $(this);
+
+        var elementIndex = $('.interview-container').index($(this));
+        var interviewContentID = elementIndex + '.html';
+        console.log(interviewContentID);
+        $('.interview-content').load(interviewContentID, function () {
+            // when finish loading, animate the content
+                    currentInterviewContainer.addClass('animate');
+                    setTimeout(function() {
+                        $('.interview-content').addClass('animate');
+
+                        $('.interview-content').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+                                  function(e) {
+
+                            $('#previews-container').addClass('no-scroll');
+
+                        });
+
+
+
+                    }, 500);
+
+
+        });
 
     });
 
