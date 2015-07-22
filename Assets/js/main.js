31a4bcf48db8db1ab14a42d3300d32f09d42204b
molased <molased@gmail.com>
2015-07-22 13:47:32 +0200

    - did integrate a dummy div to used as mediator between pages transitions  - did fix the x close button animation

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,16 +1,27 @@
 $(function () {
 
     var currentInterviewContainer;
+    var oldScrollPosition;
 
 
     $('#close-content-button').click(function () {
 
         $('.interview-content').removeClass('animate');
+        $('#close-content-button').removeClass('animate');
         $('#previews-container').removeClass('no-scroll');
 
         setTimeout(function() {
-            currentInterviewContainer.removeClass('animate');
-            $('.interview-content').html('');
+
+            $(window).scrollTop(oldScrollPosition);
+            $('.overlay').removeClass('animate');
+
+            setTimeout(function() {
+
+                currentInterviewContainer.removeClass('animate');
+                $('.interview-content').html('');
+
+            }, 500);
+
         }, 500);
     });
 
@@ -22,25 +33,31 @@ $(function () {
         var elementIndex = $('.interview-container').index($(this));
         var interviewContentID = elementIndex + '.html';
         console.log(interviewContentID);
-        $('.interview-content').load(interviewContentID, function () {
-            // when finish loading, animate the content
-                    currentInterviewContainer.addClass('animate');
+
+
+            $('.interview-content').load(interviewContentID, function () {
+                // when finish loading, animate the content
+                currentInterviewContainer.addClass('animate');
+                setTimeout(function() {
+
+                    $('.overlay').addClass('animate');
                     setTimeout(function() {
+                        oldScrollPosition = $(window).scrollTop();
+                        $(window).scrollTop(0);
                         $('.interview-content').addClass('animate');
 
                         $('.interview-content').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
-                                  function(e) {
+                                                    function(e) {
 
                             $('#previews-container').addClass('no-scroll');
+                            $('#close-content-button').addClass('animate');
 
                         });
+                    }, 700);
 
+                }, 500);
 
-
-                    }, 500);
-
-
-        });
+            });
 
     });
 
